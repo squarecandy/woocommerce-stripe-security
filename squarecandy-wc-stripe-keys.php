da935d87ac0a41cc3748d0a11a361853e5ae76a9
Peter Wise <peter@squarecandy.net>
2023-06-19 23:41:43 -0400

    chore: linting

--- squarecandy-wc-stripe-keys.php
+++ squarecandy-wc-stripe-keys.php
@@ -8,35 +8,37 @@
  */
 
 // Basic security, prevents file from being loaded directly.
-defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 // set the stripe keys via wp-config
-add_filter('option_woocommerce_stripe_settings', 'squarecandy_woocommerce_stripe_settings');
+add_filter( 'option_woocommerce_stripe_settings', 'squarecandy_woocommerce_stripe_settings' );
 function squarecandy_woocommerce_stripe_settings( $settings ) {
 
-    if ( defined( 'WC_STRIPE_TEST_PUBLISHABLE_KEY' ) ) {
-        $settings['test_publishable_key'] = WC_STRIPE_TEST_PUBLISHABLE_KEY;
-    }
+	if ( defined( 'WC_STRIPE_TEST_PUBLISHABLE_KEY' ) ) {
+		$settings['test_publishable_key'] = WC_STRIPE_TEST_PUBLISHABLE_KEY;
+	}
 
-    if ( defined( 'WC_STRIPE_TEST_SECRET_KEY' ) ) {
-        $settings['test_secret_key'] = WC_STRIPE_TEST_SECRET_KEY;
-    }
+	if ( defined( 'WC_STRIPE_TEST_SECRET_KEY' ) ) {
+		$settings['test_secret_key'] = WC_STRIPE_TEST_SECRET_KEY;
+	}
 
-    if ( defined( 'WC_STRIPE_TEST_WEBHOOK_SECRET' ) ) {
-        $settings['test_webhook_secret'] = WC_STRIPE_TEST_WEBHOOK_SECRET;
-    }
+	if ( defined( 'WC_STRIPE_TEST_WEBHOOK_SECRET' ) ) {
+		$settings['test_webhook_secret'] = WC_STRIPE_TEST_WEBHOOK_SECRET;
+	}
 
-    if ( defined( 'WC_STRIPE_PUBLISHABLE_KEY' ) ) {
-        $settings['publishable_key'] = WC_STRIPE_PUBLISHABLE_KEY;
-    }
+	if ( defined( 'WC_STRIPE_PUBLISHABLE_KEY' ) ) {
+		$settings['publishable_key'] = WC_STRIPE_PUBLISHABLE_KEY;
+	}
 
-    if ( defined( 'WC_STRIPE_SECRET_KEY' ) ) {
-        $settings['secret_key'] = WC_STRIPE_SECRET_KEY;
-    }
+	if ( defined( 'WC_STRIPE_SECRET_KEY' ) ) {
+		$settings['secret_key'] = WC_STRIPE_SECRET_KEY;
+	}
 
-    if ( defined( 'WC_STRIPE_WEBHOOK_SECRET' ) ) {
-        $settings['webhook_secret'] = WC_STRIPE_WEBHOOK_SECRET;
-    }
+	if ( defined( 'WC_STRIPE_WEBHOOK_SECRET' ) ) {
+		$settings['webhook_secret'] = WC_STRIPE_WEBHOOK_SECRET;
+	}
 
-    return $settings;
+	return $settings;
 }
