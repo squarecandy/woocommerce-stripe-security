74cb269907a40f05e13badc58000d51204a01776
Peter Wise <peter@squarecandy.net>
2023-06-21 12:27:28 -0400

    fix: refactor for env vars, make more extensible

--- squarecandy-wc-stripe-keys.php
+++ squarecandy-wc-stripe-keys.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: SquareCandy Stripe Keys Security for WooCommerce
- * Description: Allows the WC Stripe settings to be loaded from constants defined in wp-config.php
+ * Plugin Name: SquareCandy Stripe Keys Security
+ * Description: Allows the Stripe settings to be loaded from env vars or constants defined in wp-config.php
  * Author:      squarecandy
  * Plugin URI:  https://github.com/squarecandy/woocommerce-stripe-security
  * License:     GNU General Public License v3
@@ -13,32 +13,43 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// set the stripe keys via wp-config
-add_filter( 'option_woocommerce_stripe_settings', 'squarecandy_woocommerce_stripe_settings' );
-function squarecandy_woocommerce_stripe_settings( $settings ) {
+$active_plugins = (array) get_option( 'active_plugins', array() );
 
-	if ( defined( 'WC_STRIPE_TEST_PUBLISHABLE_KEY' ) ) {
-		$settings['test_publishable_key'] = WC_STRIPE_TEST_PUBLISHABLE_KEY;
+// If the WooCommerce Stripe is active
+if ( in_array( 'woocommerce-gateway-stripe/woocommerce-gateway-stripe.php', $active_plugins, true ) ) :
+
+	// set the stripe keys and webhooks via env vars or wp-config constants
+	add_filter( 'option_woocommerce_stripe_settings', 'squarecandy_woocommerce_stripe_settings' );
+	function squarecandy_woocommerce_stripe_settings( $settings ) {
+
+		$subfields = array(
+			'test_publishable_key',
+			'test_secret_key',
+			'publishable_key',
+			'secret_key',
+			'test_webhook_secret',
+			'webhook_secret',
+		);
+
+		$settings = squarecandy_iterate_settings_changes( $settings, $subfields, 'WC_STRIPE_' );
+
+		return $settings;
 	}
 
-	if ( defined( 'WC_STRIPE_TEST_SECRET_KEY' ) ) {
-		$settings['test_secret_key'] = WC_STRIPE_TEST_SECRET_KEY;
-	}
+endif;
 
-	if ( defined( 'WC_STRIPE_TEST_WEBHOOK_SECRET' ) ) {
-		$settings['test_webhook_secret'] = WC_STRIPE_TEST_WEBHOOK_SECRET;
-	}
+function squarecandy_iterate_settings_changes( $settings, $subfields, $prefix ) {
+	foreach ( $subfields as $subfield ) {
+		// construct the uppercase constant slug
+		$const_name = $prefix . mb_strtoupper( $subfield );
 
-	if ( defined( 'WC_STRIPE_PUBLISHABLE_KEY' ) ) {
-		$settings['publishable_key'] = WC_STRIPE_PUBLISHABLE_KEY;
-	}
-
-	if ( defined( 'WC_STRIPE_SECRET_KEY' ) ) {
-		$settings['secret_key'] = WC_STRIPE_SECRET_KEY;
-	}
-
-	if ( defined( 'WC_STRIPE_WEBHOOK_SECRET' ) ) {
-		$settings['webhook_secret'] = WC_STRIPE_WEBHOOK_SECRET;
+		if ( ! empty( getenv( $const_name ) ) ) {
+			// use the environment variable if it exists
+			$settings[ $subfield ] = getenv( $const_name );
+		} elseif ( defined( $const_name ) && ! empty( constant( $const_name ) ) ) {
+			// otherwise use the constant if it exists
+			$settings[ $subfield ] = constant( $const_name );
+		}
 	}
 
 	return $settings;
